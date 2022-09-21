734c569eac68c5c0294b03f85f713248e5e19dff
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-09-21 18:33:09 +0200

    Created new memory python package and module launch_game

--- Memory/help_functions.py
+++ Memory/help_functions.py
@@ -1,13 +1,19 @@
 """Define help functions for GUI menu."""
 
 import tkinter as tk
+from pathlib import Path
+from os.path import dirname, join
+
+TEXT_FOLDER = join(Path(dirname(__file__)).parent, 'text_files')
 
 
-def printRules() -> None:
+def print_rules() -> None:
     """
     Display rules and gameplay.
-    Load a text files 'rules_eng.txt' saved inside the same directory.
+
+    Load a text files 'rules_eng.txt'.
     Open a second window.
+    Write the content of the text document.
     Returns
     -------
     None.
@@ -15,26 +21,29 @@ def printRules() -> None:
     ruleWindow = tk.Toplevel()
     ruleWindow.resizable(False, False)
     ruleWindow.title("How to play?")
-    with open('rules_eng.txt') as f:
+    with open(f'{TEXT_FOLDER}/rules_eng.txt') as f:
         gameRules = f.read()
     lab_Rule = tk.Label(ruleWindow, text=gameRules,
                         fg="black", anchor="e", justify=tk.LEFT)
     lab_Rule.pack(side=tk.TOP)
     ruleWindow.mainloop()
 
+
 def about() -> None:
     """
     Display licence information.
+
     Load the text document 'about.txt'.
     Open a secondary window.
     Write the content of the text document.
+
     Returns
     -------
     None.
     """
     aboutWindow = tk.Toplevel()
     aboutWindow.title("About")
-    with open('about.txt') as f:
+    with open(f'{TEXT_FOLDER}/about.txt') as f:
         about = f.read()
     lbl_about = tk.Label(aboutWindow, text=about,
                          fg="black", anchor="e", justify=tk.LEFT)
