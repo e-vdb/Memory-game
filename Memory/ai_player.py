3c99d8c8f3bcc591ad68129905c4c0f81535d4c1
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-10-04 08:19:02 +0200

    Fix: flake8 errors.

--- Memory/ai_player.py
+++ Memory/ai_player.py
@@ -1,6 +1,5 @@
 from Memory.player import Player
 from random import choice
-import tkinter as tk
 
 
 class AIPlayer(Player):
@@ -18,7 +17,9 @@ class AIPlayer(Player):
     def play_random_cards(cards_ids):
         """AI player plays a turn."""
         first_card = choice(cards_ids)
-        second_card = choice([id_card for id_card in cards_ids if id_card != first_card])
+        second_card = choice(
+            [id_card for id_card in cards_ids if id_card != first_card]
+        )
         return first_card, second_card
 
     @staticmethod
@@ -33,12 +34,15 @@ class AIPlayer(Player):
             self.memory_board[image] = {'position': [card_id]}
 
     def search_pairs(self):
-        return next((list_ids['position'] for image, list_ids in self.memory_board.items()
+        return next((list_ids['position']
+                     for image, list_ids in self.memory_board.items()
                      if len(list_ids['position']) == 2), None)
 
     def search_matching_card(self, card_id, image):
         try:
-            return next(id_card for id_card in self.memory_board[image]['position'] if id_card != card_id)
+            return next(id_card
+                        for id_card in self.memory_board[image]['position']
+                        if id_card != card_id)
         except KeyError:
             return None
 
--- Memory/launch_game.py
+++ Memory/launch_game.py
@@ -16,8 +16,14 @@ def launch_game() -> None:
     top.add_cascade(label='Game', menu=jeu)
     submenu = tk.Menu(jeu, tearoff=False)
     jeu.add_cascade(label='New Game', menu=submenu)
-    submenu.add_command(label='Dim 5x4', command=lambda x=0: memory_game.set_dim_and_start(x))
-    submenu.add_command(label='Dim 5x6', command=lambda x=1: memory_game.set_dim_and_start(x))
+    submenu.add_command(
+        label='Dim 5x4',
+        command=lambda x=0: memory_game.set_dim_and_start(x)
+    )
+    submenu.add_command(
+        label='Dim 5x6',
+        command=lambda x=1: memory_game.set_dim_and_start(x)
+    )
     jeu.add_command(label='Close', command=window.destroy)
 
     players_menu = tk.Menu(top, tearoff=False)
@@ -25,12 +31,21 @@ def launch_game() -> None:
     one_player_menu = tk.Menu(players_menu, tearoff=False)
     players_menu.add_cascade(label='One player', menu=one_player_menu)
     one_player_menu.add_command(label='Alone', command=memory_game.play_alone)
-    one_player_menu.add_command(label='Vs Computer', command=memory_game.play_against_ai)
-    players_menu.add_command(label='2 players', command=memory_game.play_against_human)
+    one_player_menu.add_command(
+        label='Vs Computer',
+        command=memory_game.play_against_ai
+    )
+    players_menu.add_command(
+        label='2 players',
+        command=memory_game.play_against_human
+    )
 
     theme_menu = tk.Menu(top, tearoff=False)
     top.add_cascade(label='Theme', menu=theme_menu)
-    theme_menu.add_command(label='Choose theme', command=memory_game.set_up_theme_frame)
+    theme_menu.add_command(
+        label='Choose theme',
+        command=memory_game.set_up_theme_frame
+    )
 
     help_menu = tk.Menu(top, tearoff=False)
     top.add_cascade(label='Help', menu=help_menu)
